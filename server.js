77cc02c21de433e1bdd6e03b64c9414162617d6a
Mzthieu <mathilde.2silva@yahoo.com>
2025-08-28 15:38:11 +0200

    ajout de server.js (serveur local avec Express)

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+
+// Sert tous les fichiers dans "site" (HTML, CSS, images, favicons, etc.)
+app.use(express.static(path.join(__dirname, 'site')));
+
+// si l'url' est http://192.168.1.113:3000/ renvoie à site/index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'site', 'index.html'));
+});
+
+// écoute sur le port 3000
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Serveur local démarré sur http://localhost:3000/ (ip de Jules : ..., ip de Mathieu : 192.168.1.113)');
+});
