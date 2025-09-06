cf05d2e8647dfefa868192cb1279f9d3d75574c8
Mzthieu <mathilde.2silva@yahoo.com>
2025-09-06 18:45:21 +0200

    achèvement du système d'inscription

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
+
+function HashPassword(passwordToHash){
+    const salt = bcrypt.genSaltSync(saltRounds);  // génère un 'sel', qui rend le hash encore plus complexe
+    const hashedPassword = bcrypt.hashSync(passwordToHash, salt);
+    return hashedPassword;
+}
 
 // Sert tous les fichiers dans "site" (HTML, CSS, images, favicons, etc.)
 app.use(express.static(path.join(__dirname, 'site')));
+app.use(express.json());  // Permet de lire du JSON
 
 // si l'url' est http://localhost:3000/ renvoie à site/index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'site', 'index.html'));
+});
+
+app.post('/register', async (req, res) => {
+    const { mdp_hash } = req.body;
+    const hash = HashPassword(mdp_hash);
+    res.send(hash);
 });
 
 // écoute sur le port 3000
--- site/inscription.js
+++ site/inscription.js
@@ -1,15 +1,18 @@
 const eye = document.getElementById('eye');
 const eyeConfirm = document.getElementById('eye_confirm');
 const valider = document.getElementById('valider');
+const mdp = document.getElementById('mdp');
 const confirmMdp = document.getElementById('confirm');
-const bcript = require('bcrypt'); // bibliothèque pour hasher le mot de passe (peut-être un problème ici)
-const saltRounds = 10; // plus ce nombre est élevé, plus le hash sera sécurisé
 import { createClient } from 'https://esm.sh/@supabase/supabase-js';
 
 const supabaseUrl = 'https://subkyvlpofepcqbegyvq.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1Ymt5dmxwb2ZlcGNxYmVneXZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYyOTUwMzcsImV4cCI6MjA3MTg3MTAzN30.HsbZgSXl64lQMxk89adxJNhlJUdDkbvLC6HiZQmHkHM';
 const supabase = createClient(supabaseUrl, supabaseKey);
-let exec = false;
+const { data, error } = await supabase
+    .from('users')
+    .select('*')
+console.log(data);
+console.log(error);
 
 eye.addEventListener('click', () => {
     if(eye.className === "fi-xnpuxl-eye-solid"){
@@ -33,41 +36,65 @@ eyeConfirm.addEventListener('click', () => {
     window.friconix_update();
 });
 
-valider.addEventListener('click', () => {
-    alert('ervrgr');
+valider.addEventListener('click', async (e) => {
+    e.preventDefault() // empêche le rechargement
+
+    let msg_err = document.getElementById('msg_err');
     const id_connexion = document.getElementById('id_connexion');
+    if(id_connexion.value.trim() === ""){
+        msg_err.innerHTML = "⚠️ Vous n'avez entré d'identifiant de connexion !";
+        msg_err.className = 'msg_err_add';
+        return;
+    }
     const userName = document.getElementById('nom');
+    if(userName.value.trim() === ""){
+        msg_err.innerHTML = "⚠️ Vous n'avez entré de nom d'utilisateur !";
+        msg_err.className = 'msg_err_add';
+        return;
+    }
     const email = document.getElementById('email');
-    const mdp = document.getElementById('mdp');
-    let msg_err = document.getElementById('msg_err')
+    if(!email.checkValidity()){
+        document.getElementById('msg_err').innerHTML = "⚠️ L'email entré n'est pas valide !";
+        msg_err.className = 'msg_err_add';
+        return;
+    }
+    if(mdp.value.trim() === ""){
+        msg_err.innerHTML = "⚠️ Vous n'avez pas entré de mot de passe !";
+        msg_err.className = 'msg_err_add';
+        return;
+    }
+    if(confirmMdp.value.trim() === ""){
+        msg_err.innerHTML = "⚠️ Vous n'avez pas confirmé votre mot de passe !";
+        msg_err.className = 'msg_err_add';
+        return;
+    }
     if(mdp.value != confirmMdp.value){
         msg_err.innerHTML = '⚠️ Les mots de passe entrés doivent être identiques !';
         msg_err.className = 'msg_err_add';
-        exec = false;
+        return;
+    }
+    const data = {mdp_hash: mdp.value};
+    const res = await fetch("/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data)
+    });
+    const hashed = await res.text()
+    const payload = {
+        id_conn: id_connexion.value,
+        mdp_hash: hashed,
+        nom: userName.value,
+        email: email.value
+    };
+    const { error } = await supabase
+        .from('users')
+        .insert(payload)
+    if(error){
+        const message = "⚠️ Il y a eu un problème lors de la création du compte :" + JSON.stringify(error);
+        console.error("Supabase erreur : ", error);
+        alert(message);
     } else {
-        if(!email.checkValidity()){
-            document.getElementById('msg_err').innerHTML = "⚠️ L'email entrée n'est pas valide !";
-            msg_err.className = 'msg_err_add';
-            exec = false;
-        } else {
-            exec = true;
-            hash_psw = HashPassword(mdp.value); // hash (ne fonctionne pas encore)
-        }
+        alert("Votre compte a été crée avec succès !");
+        window.location.href = "http://localhost:3000/";
     }
 });
-
-async function HashPassword(passwordToHash){  // fonction censée hasher le mot de passe mais erreur avec 'await'
-    try{
-        const salt = await bcrypt.genSalt(saltRounds);  // génère un 'sel', qui rend le hash encore plus complexe
-        const hashedPassword = await bcrypt.hash(passwordToHash, salt);
-        console.log(hashedPassword)
-    }catch{
-        alert('problème, man !')
-    }
-}
-/*
-if(exec){
-    const { data, error } = await supabase
-        .from('users')
-        .insert({ id_conn: id_connexion.value, mdp_hash: mdp})
-}*/
